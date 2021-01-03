9cb9dd7e2295adb168d93560466ab26de99ef2dc
ana-vela <ana@anavela.net>
2021-01-03 01:16:51 -0700

    Add netlify admin

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,11 +1,11 @@
 module.exports = {
-  title: 'Vredeburg',
-  description: 'Eleventy starter project to create a blog',
-  keywords: ['eleventy', 'template', 'simple', 'clean'],
+  title: 'Candle Write',
+  description: 'A creative writing blog',
+  keywords: ['writing', 'poetry', 'creative writing', 'fiction'],
   // your site url without trailing slash
-  url: 'https://vredeburg.netlify.app',
+  url: 'https://candlewrite.com',
   // how many posts you want to show for each page
   paginate: 6
   // if you want to add disqus to your site
-  // disqusShortname: "your-shortname"
+  // disqusShortname: "candle-write"
 };
