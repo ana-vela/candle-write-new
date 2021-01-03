62b4bf8331dca259f0f74d0b04aad977860dcf4d
ana-vela <ana@anavela.net>
2021-01-03 01:51:22 -0700

    Add Disqus account

--- src/_data/site.js
+++ src/_data/site.js
@@ -5,7 +5,7 @@ module.exports = {
   // your site url without trailing slash
   url: 'https://candlewrite.com',
   // how many posts you want to show for each page
-  paginate: 6
+  paginate: 6,
   // if you want to add disqus to your site
-  // disqusShortname: "candle-write"
-};
+  disqusShortname: "candle-write"
+}
