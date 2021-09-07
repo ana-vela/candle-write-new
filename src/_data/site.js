c03c0fea0f71011644989141e8f158d7d7c20bd6
ana-vela <ana@anavela.net>
2021-09-06 19:55:51 -0600

    Updating site title

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,9 +1,9 @@
 module.exports = {
-  title: 'Candle Write',
+  title: 'Ana Vela',
   description: 'A creative writing blog',
   keywords: ['writing', 'poetry', 'creative writing', 'fiction'],
   // your site url without trailing slash
-  url: 'https://candlewrite.com',
+  url: 'https://anavela.me',
   // how many posts you want to show for each page
   paginate: 6,
   // if you want to add disqus to your site
